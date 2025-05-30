7a0e439377f344db00d4372676199beaa92c2ebf
le50 <leti.tec1000@gmail.com>
2025-05-30 10:46:12 -0300

    Adicionado mais métados

--- ProjetoJava/src/ClasseAlunos.java
+++ ProjetoJava/src/ClasseAlunos.java
@@ -3,4 +3,31 @@ public class ClasseAlunos {
 	String nomeAt; // Abaixo da class é atributo é não var
 	float notaAt;
 	String cursoAt;
+	
+	// Método para mostrar nome
+	public void mostrarNomeMet() {
+		System.out.println(this.nomeAt);
+	}
+	
+	// Método para mostrar nome
+		public void mostrarNotaMet() {
+			System.out.println(this.notaAt);
+		}
+		
+		// Método para mostrar nome
+		public void mostrarCursoMet() {
+			System.out.println(this.cursoAt);
+		}
+		
+		public void receberNomeMet(String nomePar) {
+			this.nomeAt = nomePar;
+		}
+		
+		public void receberNotaMet(float notaPar) {
+			this.notaAt = notaPar;
+		}
+		
+		public void receberCursoMet(String cursoPar) {
+			this.cursoAt = cursoPar;
+		}
 }
--- ProjetoJava/src/ClassePrincipal.java
+++ ProjetoJava/src/ClassePrincipal.java
@@ -7,15 +7,22 @@ public class ClassePrincipal {
 		ClasseAlunos mikaelObj = new ClasseAlunos(); // Objeto 1
 		ClasseAlunos abraaoObj = new ClasseAlunos(); // Objeto 2
 		
-		mikaelObj.nomeAt = "Mikael";
-		mikaelObj.cursoAt = "Infomatica";
-		mikaelObj.notaAt = 8.5F;
+		// Funciona, mas está errado
+		//mikaelObj.nomeAt = "Mikael";
+		//mikaelObj.cursoAt = "Infomatica";
+		//mikaelObj.notaAt = 8.5F;
 		
+		mikaelObj.receberNomeMet("Mikael");
+		mikaelObj.receberCursoMet("Informática");
+		//mikaelObj.receberNotaMet(8.5F);
 		
-		System.out.println(mikaelObj.nomeAt); // sysout CTR + SPACE
-		System.out.println(mikaelObj.cursoAt);
-		System.out.println(mikaelObj.notaAt);
-		
+		// Funciona, mas está errado
+		//System.out.println(mikaelObj.nomeAt); // sysout CTR + SPACE
+		//System.out.println(mikaelObj.cursoAt);
+		//System.out.println(mikaelObj.notaAt);
+		mikaelObj.mostrarNomeMet();
+		mikaelObj.mostrarNotaMet();
+		mikaelObj.mostrarCursoMet();
 	}
 
 }
